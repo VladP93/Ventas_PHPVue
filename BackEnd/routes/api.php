8b241e7464f100e26a7cff879fe7096a5709aaa7
DelCid98 <kevineliasdc13@gmail.com>
2021-05-12 17:32:11 -0600

    rutas

--- BackEnd/routes/api.php
+++ BackEnd/routes/api.php
@@ -19,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::resource('/roles','App\Http\Controllers\RolController');
+Route::resource('/artiulos','App\Http\Controllers\ArticuloController');
+Route::resource('/categorias','App\Http\Controllers\CategoriaController');
+Route::resource('/clientes','App\Http\Controllers\ClientesController');
+Route::resource('/detalle_ingresos','App\Http\Controllers\Detalle_ingresoController');
+Route::resource('/detalle_ventas','App\Http\Controllers\VentaController');
+Route::resource('/ingresos','App\Http\Controllers\IngresoController');
+Route::resource('/proveedors','App\Http\Controllers\ProveedorController');
+Route::resource('/usuarios','App\Http\Controllers\UsuarioController');
+Route::resource('/ventas','App\Http\Controllers\VentaController');
